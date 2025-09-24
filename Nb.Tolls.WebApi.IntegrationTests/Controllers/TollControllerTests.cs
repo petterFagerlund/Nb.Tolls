89aa9ba76dd50788c66bcede9772e058e1c80d0c
petter fagerlund <petter.fagerlund@fg.se>
2025-09-24 22:42:17 +0200

    Add test projects

---
+++ Nb.Tolls.WebApi.IntegrationTests/Controllers/TollControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Nb.Tolls.WebApi.IntegrationTests.Integration;
+using Xunit.Abstractions;
+
+namespace Nb.Tolls.WebApi.IntegrationTests.Controllers;
+
+public class TollControllerTests
+{
+    private readonly HostSutApplication _hostSutApplication;
+    private readonly HttpClient _httpClient;
+
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() }
+    };
+
+    public TollControllerTests(HostSutApplication hostSutApplication, ITestOutputHelper outputHelper)
+    {
+        _hostSutApplication = hostSutApplication;
+        hostSutApplication.Output = outputHelper;
+        hostSutApplication.InitApplication();
+        _httpClient = hostSutApplication.Client;
+    }
+}
---
+++ Nb.Tolls.WebApi.IntegrationTests/Integration/HostSutApplication.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Nb.Tolls.WebApi.IntegrationTests.Integration;
+
+[CollectionDefinition("API collection")]
+public class ApiCollection : ICollectionFixture<HostSutApplication>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
+public class HostSutApplication
+{
+    public HttpClient Client { get; private set; } = null!;
+    public ITestOutputHelper Output { get; set; } = null!;
+
+    public void InitApplication()
+    {
+        var hostApplication = new WebApplicationFactory<Program>().WithWebHostBuilder(
+            builder =>
+            {
+                builder.UseEnvironment("Integration.Test");
+                builder.ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders().AddXUnit(Output));
+
+            });
+        var client = hostApplication.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestScheme");
+        Client = client;
+    }
+}
