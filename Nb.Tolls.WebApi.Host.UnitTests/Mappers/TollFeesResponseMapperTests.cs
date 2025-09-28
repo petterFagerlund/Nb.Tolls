6f43d1272a666f989b3cdba13eeadac89abb647a
petter fagerlund <petter.fagerlund@fg.se>
2025-09-28 02:02:48 +0200

    added tests

---
+++ Nb.Tolls.WebApi.Host.UnitTests/Mappers/TollFeesResponseMapperTests.cs
@@ -0,0 +1,50 @@
+﻿using Nb.Tolls.Domain.Results;
+using Nb.Tolls.WebApi.Host.Mappers;
+using Xunit;
+
+namespace Nb.Tolls.WebApi.Host.UnitTests.Mappers;
+
+public class TollFeesResponseMapperTests
+{
+    [Fact]
+    public void Map_GivenTollFeesResult_ReturnsMappedTollFeesResponse()
+    {
+        // Arrange
+        var tollFeesResult = new DailyTollFeesResult
+        {
+            TollFees =
+            [
+                new DailyTollFeeResult { Date = new DateOnly(2025, 9, 30), TollFee = 15 },
+                new DailyTollFeeResult { Date = new DateOnly(2025, 10, 1), TollFee = 8 }
+            ]
+        };
+
+        // Act
+        var response = TollFeesResponseMapper.Map(tollFeesResult);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(2, response.TollFees.Count);
+        Assert.Equal(new DateOnly(2025, 9, 30), response.TollFees[0].TollDate);
+        Assert.Equal(15, response.TollFees[0].TollFee);
+        Assert.Equal(new DateOnly(2025, 10, 1), response.TollFees[1].TollDate);
+        Assert.Equal(8, response.TollFees[1].TollFee);
+    }
+
+    [Fact]
+    public void Map_GivenEmptyTollFeesResult_ReturnsEmptyTollFeesResponse()
+    {
+        // Arrange
+        var tollFeesResult = new DailyTollFeesResult
+        {
+            TollFees = []
+        };
+
+        // Act
+        var response = TollFeesResponseMapper.Map(tollFeesResult);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Empty(response.TollFees);
+    }
+}
---
+++ Nb.Tolls.WebApi.Host.UnitTests/Validators/TollFeesFeesRequestValidatorTests.cs
@@ -0,0 +1,75 @@
+﻿using Nb.Tolls.WebApi.Host.Validators.Implementation;
+using Xunit;
+
+namespace Nb.Tolls.WebApi.Host.UnitTests.Validators;
+
+public class TollRequestValidatorTests
+{
+    private readonly TollFeesFeesRequestValidator _sut;
+
+    public TollRequestValidatorTests()
+    {
+        _sut = new TollFeesFeesRequestValidator();
+    }
+
+    [Fact]
+    public void ValidateTollTimes_WithEmptyArray_ReturnsModelError()
+    {
+        // Arrange
+        var tollTimes = Array.Empty<DateTimeOffset>();
+
+        // Act
+        var result = _sut.ValidateTollTimes(tollTimes);
+
+        // Assert
+        Assert.False(result.IsValid);
+        Assert.True(result.ContainsKey(nameof(tollTimes)));
+        var errors = result[nameof(tollTimes)]?.Errors;
+        Assert.Single(errors!);
+        Assert.Equal("At least one toll time must be provided.", errors![0].ErrorMessage);
+    }
+
+    [Fact]
+    public void ValidateTollTimes_WithFutureDate_AddsModelError()
+    {
+        // Arrange
+        var futureTime = DateTimeOffset.UtcNow.AddHours(1);
+        var tollTimes = new[] { futureTime };
+
+        // Act
+        var result = _sut.ValidateTollTimes(tollTimes);
+
+        // Assert
+        Assert.False(result.IsValid);
+        Assert.Contains(nameof(tollTimes), result.Keys);
+        Assert.Contains(result[nameof(tollTimes)]?.Errors!, e => e.ErrorMessage.Contains("must not be in the future"));
+    }
+
+    [Fact]
+    public void ValidateTollTimes_WithMaxOrMinDate_AddsModelError()
+    {
+        // Arrange
+        var tollTimes = new[] { DateTimeOffset.MaxValue, DateTimeOffset.MinValue };
+
+        // Act
+        var result = _sut.ValidateTollTimes(tollTimes);
+
+        // Assert
+        Assert.False(result.IsValid);
+        Assert.Contains(nameof(tollTimes), result.Keys);
+        Assert.Equal(3, result[nameof(tollTimes)]!.Errors.Count);
+    }
+
+    [Fact]
+    public void ValidateTollTimes_WithValidDates_ReturnsValidModelState()
+    {
+        // Arrange
+        var tollTimes = new[] { DateTimeOffset.UtcNow.AddMinutes(-10), DateTimeOffset.UtcNow.AddMinutes(-5) };
+
+        // Act
+        var result = _sut.ValidateTollTimes(tollTimes);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
