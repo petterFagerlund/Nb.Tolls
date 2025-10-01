2c1f393aa84bf32d5bc09895908b07821e703166
petter fagerlund <petter.fagerlund@fg.se>
2025-10-01 21:45:55 +0200

    remove unecessary config properties

--- Nb.Tolls.Infrastructure/Models/TollFeeConfigData.cs
+++ Nb.Tolls.Infrastructure/Models/TollFeeConfigData.cs
@@ -2,7 +2,5 @@
 
 internal class TollFeeConfigData
 {
-    public required string Timezone { get; init; }
-    public required string Semantics { get; init; }
     public required List<TollFeesModel> TollFees { get; init; }
 }
